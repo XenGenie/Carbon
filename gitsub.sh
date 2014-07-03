f701e36c85e8dbe68eaca4f1c6d3004eb3a847d7
MidKnight Knerd <xopherdeep@users.noreply.github.com>
2014-07-02 23:42:21 -0700

    Update gitsub.sh

--- gitsub.sh
+++ gitsub.sh
@@ -4,8 +4,8 @@ do
 	pattern='^.*[^/]'    #Regular expressions are "greedy", they'll match the longest substring they can from the 
 	[[ $x =~ $pattern ]]   #Apply regex
 	x=${BASH_REMATCH[0]}
-    printf "Remove  %s\n" $x
+	printf "Remove  %s\n" $x
 	git rm $x -r
 	printf "Adding submodule %s .git\n" $x
-    git submodule add -b master -f https://github.com/SuperDomX/$x $x
+	git submodule add -b master -f https://github.com/SuperDomX/$x $x
 done
