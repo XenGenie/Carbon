1fcd7e1609f8261d77eb6108c28df9473353b79f
xopherdeep <heylisten@xtiv.net>
2014-07-02 23:32:51 -0700

    Updated gitsub.sh

--- gitsub.sh
+++ gitsub.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 for x in $(ls -d */)
 do 
+	pattern='^.*[^/]'    #Regular expressions are "greedy", they'll match the longest substring they can from the 
+	[[ $x =~ $pattern ]]   #Apply regex
+	echo ${BASH_REMATCH[0]}   #Print the portion of the source string which matched the regex.
+	x=${BASH_REMATCH[0]}
+
     printf "Remove  %s\n" $x
-	git rm $x
-	printf "Adding submodule %s\n" $x
+	git rm $x -r
+	rm $x -r
+	printf "Adding submodule %s .git\n" $x
     git submodule add -b master -f https://github.com/SuperDomX/$x $x
 done
